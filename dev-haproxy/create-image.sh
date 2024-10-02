df05d8a825fe6c80404f032793f7b0d34b320a0f
Patrick Dowler <pdowler.cadc@gmail.com>
2024-10-02 09:34:08 -0700

    dev-haproxy: update to fedora 40 base; no proxy cert support

--- dev-haproxy/create-image.sh
+++ dev-haproxy/create-image.sh
@@ -2,49 +2,44 @@
 
 . ../scripts/wait-for-network.sh
 
-IMG=dev-haproxy
-IMGFILE=${IMG}.tar.gz
-CTR=tmp-$(uuidgen)
+CTR=haproxy-f40
 
-lxc list $CTR | grep -q $CTR
+incus list $CTR | grep -q $CTR
 if [ $? == 0 ]; then
 	echo "found: $CTR"
 else
-	lxc init images:centos/7 $CTR
+	incus init images:fedora/40 $CTR
 	echo "created: $CTR"
 fi
 
 # must be running to install and configure
 echo "starting $CTR ..."
-lxc start $CTR
+incus start $CTR
 
 waitForNetwork $CTR
 
 echo "installing haproxy ..."
-lxc exec $CTR -- yum -y install haproxy
+incus exec $CTR -- dnf -y install haproxy
+incus exec $CTR -- dnf clean all
 
-echo "inject custom systemd unit for HAProxy to enable x509 proxy certificates"
-lxc exec $CTR mkdir /etc/systemd/system/haproxy.service.d
-lxc file push system/override.conf $CTR/etc/systemd/system/haproxy.service.d/
+echo "inject config: $cf"
+cat config/haproxy.cfg config/haproxy-rules.conf config/haproxy-backends.conf > tmp.cfg
+incus file push tmp.cfg $CTR/etc/haproxy/haproxy.cfg
+\rm tmp.cfg
 
-for cf in config/*.*; do 
-    echo "inject config: $cf"
-    lxc file push $cf $CTR/etc/haproxy/
+incus file push $HOME/work/dev-server-cert.pem $CTR/etc/haproxy/
+
+echo "adding CA certs"
+for f in $HOME/work/etc/cacerts/*; do
+  incus file push $f $CTR/etc/pki/ca-trust/source/anchors/
 done
+incus exec $CTR update-ca-trust
 
-echo "enable HAproxy at next boot"
-lxc exec $CTR systemctl enable haproxy
+echo "enable HAproxy"
+incus exec $CTR systemctl enable haproxy
+incus exec $CTR systemctl start haproxy
 
-lxc exec $CTR -- yum clean all
-
-#echo "stopping $CTR ..."
-lxc stop $CTR
-
-## publish container as a new image in local image store
-echo "publishing $CTR ..."
-lxc publish $CTR --alias $IMG description="Centos7 + HAProxy"
-lxc delete $CTR
-echo "published: $IMG"
+incus exec $CTR systemctl status haproxy
 
 
 
